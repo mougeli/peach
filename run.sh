1b5521505dca2526adff323018f10a5cb0eff885
mougeli <lizhengqiang@mouge.wang>
2016-03-03 14:48:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 export PEACH=$GOPATH/src/peach/peach
-$PEACH new --target=/$APP.peach
+$PEACH new --target=/$APP.peach --yes
+rm /$APP.peach/custom -fr
 git clone $GIT /$APP.peach/custom
 pushed /$APP.peach 
 $PEACH web
